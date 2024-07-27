2db5f8849118911d6cd911e0fd9c17dbef37b23d
muratanaoki <51051386+muratanaoki@users.noreply.github.com>
2024-07-27 21:56:27 +0900

    修正

--- StudyWall/Views/ContentView.swift
+++ StudyWall/Views/ContentView.swift
@@ -7,41 +7,41 @@ struct ContentView: View {
         Wallpaper(imageName: "wallpaper3", title: "Phrases 1", category: "Phrases"),
         Wallpaper(imageName: "wallpaper4", title: "Phrases 2", category: "Phrases")
     ]
-    
+
     var categories: [String: [Wallpaper]] {
         Dictionary(grouping: wallpapers, by: { $0.category })
     }
-    
+
     var body: some View {
         NavigationView {
             ScrollView {
                 ForEach(categories.keys.sorted(), id: \.self) { key in
                     Section(header: Text(key)
-                                .font(.headline)
-                                .padding(.leading, 15)
-                                .padding(.top, 10)) {
-                        ScrollView(.horizontal, showsIndicators: false) {
-                            HStack {
-                                ForEach(categories[key]!) { wallpaper in
-                                    NavigationLink(destination: WallpaperDetailView(wallpaper: wallpaper)) {
-                                        VStack {
-                                            Image(wallpaper.imageName)
-                                                .resizable()
-                                                .aspectRatio(contentMode: .fill)
-                                                .frame(width: 150, height: 150)
-                                                .clipped()
-                                                .cornerRadius(10)
-                                            Text(wallpaper.title)
-                                                .font(.caption)
-                                                .padding(.top, 5)
+                        .font(.headline)
+                        .padding(.leading, 15)
+                        .padding(.top, 10)) {
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                HStack {
+                                    ForEach(categories[key]!) { wallpaper in
+                                        NavigationLink(destination: WallpaperDetailView(wallpaper: wallpaper)) {
+                                            VStack {
+                                                Image(wallpaper.imageName)
+                                                    .resizable()
+                                                    .aspectRatio(contentMode: .fill)
+                                                    .frame(width: 150, height: 150)
+                                                    .clipped()
+                                                    .cornerRadius(10)
+                                                Text(wallpaper.title)
+                                                    .font(.caption)
+                                                    .padding(.top, 5)
+                                            }
+                                            .padding(.leading, 15)
                                         }
-                                        .padding(.leading, 15)
                                     }
                                 }
                             }
+                            .frame(height: 200)
                         }
-                        .frame(height: 200)
-                    }
                 }
             }
             .navigationTitle("Study Wall")
