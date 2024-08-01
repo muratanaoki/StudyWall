547ce99bbf71cc7ed6cc4cfc320641944026cf06
muratanaoki <51051386+muratanaoki@users.noreply.github.com>
2024-08-01 20:15:15 +0900

    修正

--- StudyWall/Views/Homes/Home.swift
+++ StudyWall/Views/Homes/Home.swift
@@ -1,18 +1,51 @@
-//
-//  Home.swift
-//  StudyWall
-//
-//  Created by 村田直紀 on 2024/07/31.
-//
+/*
+See the LICENSE.txt file for this sample’s licensing information.
+
+Abstract:
+A view showing featured landmarks above a list of landmarks grouped by category.
+*/
 
 import SwiftUI
 
 struct Home: View {
+    @Environment(ModelData.self) var modelData
+    @State private var showingProfile = false
+
     var body: some View {
-        VStack {Text("ddAA")}
+        NavigationSplitView {
+            List {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
+                }
+                .listRowInsets(EdgeInsets())
+            }
+            .listStyle(.inset)
+            .navigationTitle("Featured")
+            .toolbar {
+                Button {
+                    showingProfile.toggle()
+                } label: {
+                    Label("User Profile", systemImage: "person.crop.circle")
+                }
+            }
+            .sheet(isPresented: $showingProfile) {
+                ProfileHost()
+                    .environment(modelData)
+            }
+        } detail: {
+            Text("Select a Landmark")
+        }
     }
 }
 
 #Preview {
     Home()
+        .environment(ModelData())
 }
